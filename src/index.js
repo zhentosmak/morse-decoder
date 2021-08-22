b78c9d788ab4776365d5fb8fc708691f2a99de22
zhentosmak <zhentosmak@mail.ru>
2021-08-22 15:23:33 +0300

    init: start youtube-task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const morseDecode = {}
+    const dotValue = '10'
+    const dasheValue = '11'
+    for(let item in MORSE_TABLE) {
+      let newItemName = ''
+      if (item.length*2 < 10) {
+        for (let i = 10 - item.length*2; i > 0; i--) {
+          newItemName += '0'
+        }
+      }
+      for (let i = 0; i < item.length; i++) {
+        if (item[i] === '-') {
+          newItemName += dasheValue
+        } else {
+          newItemName += dotValue
+        }
+      }
+      morseDecode[newItemName] = MORSE_TABLE[item]
+    }
+    const phrase = expr.match( /.{1,10}/g );
+    let string = ''
+    phrase.forEach((item, i, arr) => {
+      if (item === '**********') {
+        string += ' '
+      } else {
+        for (let key in morseDecode) {
+          if (item == key) {
+            string += morseDecode[key]
+          }
+        }
+      }
+    })
+    return string
 }
 
 module.exports = {
